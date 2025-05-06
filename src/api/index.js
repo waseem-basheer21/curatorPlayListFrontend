2de76782d22808c706f2523b55ccb6355bbfb302
waseem <v867610@gmail.com>
2025-05-06 13:18:28 +0530

    add env

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:1337/api', 
+  baseURL: 'process.env.REACT_APP_API_URL', 
   headers: {
     'Content-Type': 'application/json',
   },
